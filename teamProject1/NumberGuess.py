30efb1dd83c67ed4c41664308ab4436ba56b0c69
CuChulainn64 <patmiller21337@gmail.com>
2023-01-11 14:49:41 -0500

    added NumGame class

---
+++ teamProject1/NumberGuess.py
@@ -0,0 +1,15 @@
+#The computer selects a random number in a range, and the
+#player has to guess the number.  The player has “n” number of guesses, and the
+#computer will give hints about the number as the user guesses.  Try and come up
+#with your own implementation
+class NumGame:
+    def __init__(self):
+        pass
+    def startGame(self):
+        pass
+    def playerguess(num):
+        pass
+    def giveHint():
+        pass
+    def gameOver(menu):
+        pass
---
+++ teamProject1/main.py
@@ -0,0 +1,14 @@
+#The computer selects a random number in a range, and the
+#player has to guess the number.  The player has “n” number of guesses, and the
+#computer will give hints about the number as the user guesses.  Try and come up
+#with your own implementation
+
+#Error handling to keep the application from crashing (Errors and Exceptions)
+#A mainfile where users can access a menuto select the game to play (File Reading)
+#The results of the games should be persisted in a log file (File Reading)
+#Game logic separated into moduleswhere it can be easily maintained (Modules)
+#Use at least one class in each game (OOP)
+print("1| Rock paper scissors")
+print("2| Number guess game")
+print("3| Tic tac toe")
+selection = input("Hello, please select a game: ")
