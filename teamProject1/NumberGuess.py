62744f1c0f205ab8ef1989a9a073efe210212305
CuChulainn64 <patmiller21337@gmail.com>
2023-01-11 15:28:57 -0500

    basic logic done

--- teamProject1/NumberGuess.py
+++ teamProject1/NumberGuess.py
@@ -2,14 +2,29 @@
 #player has to guess the number.  The player has “n” number of guesses, and the
 #computer will give hints about the number as the user guesses.  Try and come up
 #with your own implementation
+import random
 class NumGame:
-    def __init__(self):
-        pass
+    def __init__(self, rounds, min, max):
+        self.numberRounds = rounds
+        self.maxNumber = max
+        self.minNumber = min
+        self.__randomNumber = random.randint(min, max)
+    
     def startGame(self):
-        pass
-    def playerguess(num):
-        pass
-    def giveHint():
-        pass
-    def gameOver(menu):
-        pass
+        for i in range(1, self.numberRounds):
+            if(self.playerGuess() == self.__randomNumber):
+                self.gameOver("You Win")
+            else:
+                self.giveHint(self.previousGuess)
+        self.gameOver(f"Sorry, the number was {self.__randomNumber}. You Lose")
+    def playerguess(self):
+        guess = input("Guess a number: ")
+        self.previousGuess = int(guess)
+        return guess
+    def giveHint(self, num):
+        if(num > self.__randomNumber):
+            print("smaller")
+        else:
+            print("bigger")
+    def gameOver(result):
+        print(result)
