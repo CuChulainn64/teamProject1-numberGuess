4ee573afc81d7417d6ffeb3e9fef932227d197f5
CuChulainn64 <patmiller21337@gmail.com>
2023-01-12 10:18:57 -0500

    fixed some bugs

--- teamProject1/NumberGuess.py
+++ teamProject1/NumberGuess.py
@@ -6,28 +6,37 @@ import random
 import yaml
 class NumGame:
     def __init__(self, rounds, min, max):
-        self.numberRounds = rounds
-        self.maxNumber = max
-        self.minNumber = min
-        self.__randomNumber = random.randint(min, max)
+        try:
+            self.numberRounds = rounds
+            self.maxNumber = max
+            self.minNumber = min
+            self.__randomNumber = random.randint(min, max)
+        except ValueError:
+            print("invalid starting values")
+        self.__startGame()
     
-    def startGame(self):
-        for i in range(1, self.numberRounds):
-            if(self.playerGuess() == self.__randomNumber):
-                self.gameOver(f"You Win. It only took {i} guesses")
+    def __startGame(self):
+        for i in range(1, self.numberRounds+1):
+            guess = self.playerGuess()
+            if( guess == self.__randomNumber):
+                result = f"You Win. It only took {i} guesses"
+                break
             else:
                 self.giveHint(self.previousGuess)
-        self.gameOver(f"Sorry, the number was {self.__randomNumber}. You Lose")
-    def playerguess(self):
+                result = f"Sorry, the number was {self.__randomNumber}. You Lose"
+        self.gameOver(result)
+    def playerGuess(self):
         guess = input("Guess a number: ")
         self.previousGuess = int(guess)
-        return guess
+        return int(guess)
     def giveHint(self, num):
         if(num > self.__randomNumber):
             print("smaller")
         else:
             print("bigger")
-    def gameOver(result):
+    def gameOver(self, result):
         with open("Nuber guess log.txt", 'a') as file:
             file.write(yaml.dump(result) + '\n')
         print(result)
+        
+NumGame(1, 1, 1)
